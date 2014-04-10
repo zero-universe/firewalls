3da2c7ba7d85283719f2f4024470c38c10ee0756
root <root@arch-nft.kvms.grafmurr.de>
2014-04-10 21:04:40 +0200

    adjusted rules and cleaned up - a bit

--- nft.standalone.sh
+++ nft.standalone.sh
@@ -23,21 +23,21 @@ table filter {
 	chain my_tcpv4 {
 		
 		# bad tcp -> avoid network scanning:
-        meta iif wlp2s0 tcp flags & (fin|syn) == (fin|syn) drop
-        meta iif wlp2s0 tcp flags & (syn|rst) == (syn|rst) drop
-        meta iif wlp2s0 tcp flags & (fin|syn|rst|psh|ack|urg) < (fin) drop # == 0 would be better, not supported yet.
-        meta iif wlp2s0 tcp flags & (fin|syn|rst|psh|ack|urg) == (fin|psh|urg) drop
+        tcp flags & (fin|syn) == (fin|syn) drop
+        tcp flags & (syn|rst) == (syn|rst) drop
+        tcp flags & (fin|syn|rst|psh|ack|urg) < (fin) drop # == 0 would be better, not supported yet.
+        tcp flags & (fin|syn|rst|psh|ack|urg) == (fin|psh|urg) drop
 		
-		meta iif wlp2s0 ct state {established, related} accept
+		ct state {established, related} accept
         
         # invalid connections
-		meta iif wlp2s0 ct state invalid drop
+		ct state invalid drop
 
 		# loopback interface
 		meta iif lo accept
 
 		# open tcp ports: sshd (22)
-		meta iif wlp2s0 tcp dport { 22 } accept
+		meta iif ens3 tcp dport { 22 } counter accept
 
 		# everything else
 		reject
@@ -49,7 +49,7 @@ table filter {
 		ct state {established, related} accept
         
         # invalid connections
-		meta iif wlp2s0 ct state invalid drop
+		ct state invalid drop
 
 		# loopback interface
 		meta iif lo accept
@@ -61,19 +61,19 @@ table filter {
          
             
 	chain my_icmpv4 {
-		meta iif wlp2s0 ct state {established, related} accept
+		ct state {established, related} accept
         
         # invalid connections
-		meta iif wlp2s0 ct state invalid drop
+		ct state invalid drop
 
 		# loopback interface
 		meta iif lo accept
 
-		meta iif wlp2s0 icmp type { echo-request, echo-reply, destination-unreachable } accept
-		#meta iif wlp2s0 icmp type echo-request accept
-		#meta iif wlp2s0 icmp type echo-reply accept
-		#meta iif wlp2s0 icmp type destination-unreachable accept
-		#meta iif wlp2s0 limit rate 5/second counter accept
+		meta iif ens3 icmp type { echo-request, echo-reply, destination-unreachable } counter accept
+		#meta iif ens3 icmp type echo-request accept
+		#meta iif ens3 icmp type echo-reply accept
+		#meta iif ens3 icmp type destination-unreachable accept
+		meta iif ens3 limit rate 10/second counter accept
 
 		# everything else
 		reject
@@ -89,12 +89,12 @@ table filter {
 	chain output { 
 		type filter hook output priority 0; 
 		
-		meta iif wlp2s0 ct state {new, established, related} accept
+		#ct state {new, established, related} counter accept
 		
-		meta iif wlp2s0 tcp dport { 22, 53 } counter accept
-		meta iif wlp2s0 udp dport { 53 } counter accept
+		#meta iif ens3 tcp dport { 22, 53 } counter accept
+		#meta iif ens3 udp dport { 53 } counter accept
 		#accept
-		counter drop
+		accept
 		}
 		
 }
