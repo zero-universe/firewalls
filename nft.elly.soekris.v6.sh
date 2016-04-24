acd7baeac4e22979d81c60c84e99195259f9dd08
Oleg Lusan <zero.universe@gmail.com>
2016-04-24 22:07:16 +0200

    updated/adjusted some more rules

--- nft.elly.soekris.v6.sh
+++ nft.elly.soekris.v6.sh
@@ -13,7 +13,7 @@
 # eth3 = kvms
 # tun1 = ziont
 
-table filter {
+table ip6 filter {
 	
 	chain input	{ 
 		type filter hook input priority 0; policy drop; 
@@ -28,34 +28,34 @@ table filter {
 		ct state {established, related} accept
 
 		# incoming inet trafic
-		iif eth0 ip protocol tcp goto my_world_tcpv4
-		iif eth0 ip protocol udp goto my_world_udpv4
-		iif eth0 ip protocol icmp goto my_world_icmpv4
+		iif eth0 ip protocol tcp goto my_world_tcpv6
+		iif eth0 ip protocol udp goto my_world_udpv6
+		iif eth0 ip protocol icmp goto my_world_icmpv6
 		
-		iif eth1 ip protocol tcp goto my_lan_tcpv4
-		iif eth1 ip protocol udp goto my_lan_udpv4
-		iif eth1 ip protocol icmp goto my_lan_icmpv4
+		iif eth1 ip protocol tcp goto my_lan_tcpv6
+		iif eth1 ip protocol udp goto my_lan_udpv6
+		iif eth1 ip protocol icmp goto my_lan_icmpv6
 		
-		iif eth2 ip protocol tcp goto my_gwlan_tcpv4
-		iif eth2 ip protocol udp goto my_gwlan_udpv4
-		iif eth2 ip protocol icmp goto my_gwlan_icmpv4
+		iif eth2 ip protocol tcp goto my_gwlan_tcpv6
+		iif eth2 ip protocol udp goto my_gwlan_udpv6
+		iif eth2 ip protocol icmp goto my_gwlan_icmpv6
 		
-		iif eth3 ip protocol tcp goto my_kvms_tcpv4
-		iif eth3 ip protocol udp goto my_kvms_udpv4
-		iif eth3 ip protocol icmp goto my_kvms_icmpv4
+		iif eth3 ip protocol tcp goto my_kvms_tcpv6
+		iif eth3 ip protocol udp goto my_kvms_udpv6
+		iif eth3 ip protocol icmp goto my_kvms_icmpv6
 		
-		iif wlan0 ip protocol tcp goto my_wlan_tcpv4
-		iif wlan0 ip protocol udp goto my_wlan_udpv4
-		iif wlan0 ip protocol icmp goto my_wlan_icmpv4
+		iif wlan0 ip protocol tcp goto my_wlan_tcpv6
+		iif wlan0 ip protocol udp goto my_wlan_udpv6
+		iif wlan0 ip protocol icmp goto my_wlan_icmpv6
 		
-		iif tun1 ip protocol tcp goto my_ziont_tcpv4
-		iif tun1 ip protocol udp goto my_ziont_udpv4
-		iif tun1 ip protocol icmp goto my_ziont_icmpv4
+		iif tun1 ip protocol tcp goto my_ziont_tcpv6
+		iif tun1 ip protocol udp goto my_ziont_udpv6
+		iif tun1 ip protocol icmp goto my_ziont_icmpv6
 
 		}
 		
 		
-	chain my_world_tcpv4 {
+	chain my_world_tcpv6 {
 		
 		# invalid connections
 		ct state invalid drop
@@ -67,18 +67,18 @@ table filter {
 		ct state {established, related} accept
 		
 		# bad tcp -> avoid network scanning:
-        tcp flags & (fin|syn) == (fin|syn) drop
-        tcp flags & (syn|rst) == (syn|rst) drop
-        tcp flags & (fin|syn|rst|psh|ack|urg) == 0 drop
-        tcp flags & (fin|syn|rst|psh|ack|urg) == (fin|psh|urg) drop
+		tcp flags & (fin|syn) == (fin|syn) drop
+		tcp flags & (syn|rst) == (syn|rst) drop
+		tcp flags & (fin|syn|rst|psh|ack|urg) == 0 drop 
+		tcp flags & (fin|syn|rst|psh|ack|urg) == (fin|psh|urg) drop
 
-		# open tcp ports: openvpn, sshd
-		iif eth0 tcp dport { 1195, 23235 } accept
+		# open tcp ports: sec
+		iif eth0 tcp dport { 23235 } accept
 
         }
 	
 	
-	chain my_world_udpv4 {
+	chain my_world_udpv6 {
 
 		# invalid connections
 		ct state invalid drop
@@ -92,7 +92,7 @@ table filter {
         }
          
             
-	chain my_world_icmpv4 {
+	chain my_world_icmpv6 {
 
 		# invalid connections
 		ct state invalid drop
@@ -103,13 +103,13 @@ table filter {
 		# established/related connections
 		ct state {established, related} accept
 
-		iif eth0 icmp type { echo-request, echo-reply, destination-unreachable, fragmentation-needed, parameter-problem } counter accept
+		iif eth0 icmp type { echo-request, echo-reply, destination-unreachable, packet-too-big, time-exceeded, parameter-problem, router-solicitation, router-advertisement, neighbor-solicitation, neighbor-advertisement, 141, 142, 151, 152, 153  } counter accept
 		iif eth0 limit rate 10/second counter accept
     
         }
 
 
-	chain my_lan_tcpv4 {
+	chain my_lan_tcpv6 {
 	
 		# invalid connections
 		ct state invalid drop
@@ -121,10 +121,10 @@ table filter {
 		ct state {established, related} accept
 		
 		# bad tcp -> avoid network scanning:
-        tcp flags & (fin|syn) == (fin|syn) drop
-        tcp flags & (syn|rst) == (syn|rst) drop
-        tcp flags & (fin|syn|rst|psh|ack|urg) == 0 drop
-        tcp flags & (fin|syn|rst|psh|ack|urg) == (fin|psh|urg) drop
+		tcp flags & (fin|syn) == (fin|syn) drop
+		tcp flags & (syn|rst) == (syn|rst) drop
+		tcp flags & (fin|syn|rst|psh|ack|urg) == 0 drop 
+		tcp flags & (fin|syn|rst|psh|ack|urg) == (fin|psh|urg) drop
 
 		# open tcp ports: dns,dhcp,ntp,squid,tor,sec
 		iif eth1 tcp dport { 53,67,68,123,3128,9060,23235 } accept
@@ -132,7 +132,7 @@ table filter {
         }
 	
 	
-	chain my_lan_udpv4 {
+	chain my_lan_udpv6 {
 
 		# invalid connections
 		ct state invalid drop
@@ -149,7 +149,7 @@ table filter {
         }
          
             
-	chain my_lan_icmpv4 {
+	chain my_lan_icmpv6 {
 
 		# invalid connections
 		ct state invalid drop
@@ -160,13 +160,13 @@ table filter {
 		# established/related connections
 		ct state {established, related} accept
 		
-		iif eth1 icmp type { echo-request, echo-reply, destination-unreachable, fragmentation-needed, parameter-problem } counter accept
+		iif eth1 icmp type { echo-request, echo-reply, destination-unreachable, packet-too-big, time-exceeded, parameter-problem, router-solicitation, router-advertisement, neighbor-solicitation, neighbor-advertisement, 141, 142, 151, 152, 153  } counter accept
 		iif eth1 limit rate 10/second counter accept
     
         }
 
 
-	chain my_gwlan_tcpv4 {
+	chain my_gwlan_tcpv6 {
 
 		# invalid connections
 		ct state invalid drop
@@ -189,7 +189,7 @@ table filter {
         }
 	
 	
-	chain my_gwlan_udpv4 {
+	chain my_gwlan_udpv6 {
 
 		# invalid connections
 		ct state invalid drop
@@ -206,7 +206,7 @@ table filter {
         }
          
             
-	chain my_gwlan_icmpv4 {
+	chain my_gwlan_icmpv6 {
 
 		# invalid connections
 		ct state invalid drop
@@ -217,13 +217,13 @@ table filter {
 		# established/related connections
 		ct state {established, related} accept
 		
-		iif eth2 icmp type { echo-request, echo-reply, destination-unreachable, fragmentation-needed, parameter-problem } counter accept
+		iif eth2 icmp type { echo-request, echo-reply, destination-unreachable, packet-too-big, time-exceeded, parameter-problem, router-solicitation, router-advertisement, neighbor-solicitation, neighbor-advertisement, 141, 142, 151, 152, 153  } counter accept
 		iif eth2 limit rate 3/second counter accept
     
         }
         
         
-	chain my_kvms_tcpv4 {
+	chain my_kvms_tcpv6 {
 
 		# invalid connections
 		ct state invalid drop
@@ -246,7 +246,7 @@ table filter {
         }
 	
 	
-	chain my_kvms_udpv4 {
+	chain my_kvms_udpv6 {
 
 		# invalid connections
 		ct state invalid drop
@@ -263,15 +263,15 @@ table filter {
         }
          
             
-	chain my_kvms_icmpv4 {
+	chain my_kvms_icmpv6 {
 
-		iif eth3 icmp type { echo-request, echo-reply, destination-unreachable, fragmentation-needed, parameter-problem } counter accept
+		iif eth3 icmp type { echo-request, echo-reply, destination-unreachable, packet-too-big, time-exceeded, parameter-problem, router-solicitation, router-advertisement, neighbor-solicitation, neighbor-advertisement, 141, 142, 151, 152, 153  } counter accept
 		iif eth3 limit rate 3/second counter accept
     
         }
         
         
-	chain my_wlan_tcpv4 {
+	chain my_wlan_tcpv6 {
 
 		# invalid connections
 		ct state invalid drop
@@ -294,7 +294,7 @@ table filter {
         }
 	
 	
-	chain my_wlan_udpv4 {
+	chain my_wlan_udpv6 {
 
 		# invalid connections
 		ct state invalid drop
@@ -311,7 +311,7 @@ table filter {
         }
          
             
-	chain my_wlan_icmpv4 {
+	chain my_wlan_icmpv6 {
 
 		# invalid connections
 		ct state invalid drop
@@ -322,7 +322,7 @@ table filter {
 		# established/related connections
 		ct state {established, related} accept
 		
-		iif wlan0 icmp type { echo-request, echo-reply, destination-unreachable, fragmentation-needed, parameter-problem } counter accept
+		iif wlan0 icmp type { echo-request, echo-reply, destination-unreachable, packet-too-big, time-exceeded, parameter-problem, router-solicitation, router-advertisement, neighbor-solicitation, neighbor-advertisement, 141, 142, 151, 152, 153  } counter accept
 		iif wlan0 limit rate 3/second counter accept
     
         }
@@ -331,12 +331,6 @@ table filter {
 	chain output { 
 		type filter hook output priority 0; policy accept;
 
-		# invalid connections
-		ct state invalid drop
-
-		# loopback interface
-		oif lo accept
-		
 		}
 		
 		
@@ -365,7 +359,7 @@ table filter {
 }
 
 
-table nat {
+table ip6 nat {
 
 	chain prerouting {
 		type nat hook prerouting priority -150;
@@ -377,12 +371,7 @@ table nat {
 		type nat hook postrouting priority -150; policy accept;
 		
 		oif eth0 masquerade
-		
-		# snat
-		#ip saddr 192.168.77.0/24 oif eth0 snat 1.2.3.4
-		#ip saddr 192.168.88.0/24 oif eth0 snat 1.2.3.4
-		#ip saddr 192.168.99.0/24 oif eth0 snat 1.2.3.4
-		
+
 		}
 
 }
